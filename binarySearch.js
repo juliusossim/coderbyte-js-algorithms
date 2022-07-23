ee490490c66a008862e935dcb99203308a5ea93b
julius-ossim-waya <105108238+julius-ossim-waya@users.noreply.github.com>
2022-07-23 14:40:20 +0100

    recursive binary search done

--- binarySearch.js
+++ binarySearch.js
@@ -1,11 +1,13 @@
 // fast way to search a sorted array
 
+//iterative method
+
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 19];
 
 const binarySearch = ({ arr, target }) => {
   let left = 0, right = arr.length - 1;
   let mid = Math.floor(left + right / 2);
-  
+
   while(left < right){
     if(target === arr[mid]){
       return mid;
@@ -20,4 +22,15 @@ const binarySearch = ({ arr, target }) => {
   return false;
 };
 
-console.log(binarySearch({arr, target: 12}));
+
+//recursive method
+const binarySearchRecursive = ({ arr, target, left, right }) => {
+  const mid = Math.floor(left + right / 2);
+  if (target === arr[mid]) return mid;
+  if(target > left)
+    binarySearchRecursive({arr, target, left: mid + 1, right});
+  else  binarySearchRecursive({arr, target, left: left, right: mid - 1});
+  return false;
+
+};
+console.log(binarySearchRecursive({arr, target: 12, left: 0, right: 7}));
